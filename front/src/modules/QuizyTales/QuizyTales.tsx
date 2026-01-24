6f0d0b5d811cd7da735629c1d538921665dd14d2
Alexey917 <pochtovii.1996@mail.ru>
2026-01-24 16:28:00 +0300

    главный экран адаптирован под 1200

--- front/src/modules/QuizyTales/QuizyTales.tsx
+++ front/src/modules/QuizyTales/QuizyTales.tsx
@@ -1,26 +1,37 @@
+import { useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { CustomLink, Logo, CustomButton, BrandedLettering } from '@/ui';
 
 import classes from './QuizyTales.module.css';
 import title from '../../assets/sprite.svg';
 
 export const QuizyTales = () => {
+  const isDesktop = useMediaQuery({ maxWidth: 1200 });
+
+  const getLinkProps = () => {
+    if (isDesktop)
+      return { width: '277px', fontSize: '20px', padding: '19px 45px' };
+
+    return { width: '285px', fontSize: '24px', padding: '24px 33px' };
+  };
+
+  const getButtonProps = () => {
+    if (isDesktop)
+      return { width: '277px', fontSize: '20px', padding: '20px 26px' };
+
+    return { width: '285px', fontSize: '24px', padding: '28px 22px' };
+  };
+
   return (
     <section className={classes.section} aria-labelledby="quizy-tales-title">
       <div className={classes.container}>
-        <Logo width={590} height={536} aria-label="Логотип QuizyTales" />
+        <Logo aria-label="Логотип QuizyTales" />
         <div className={classes.contentWrapper}>
           <h1 id="quizy-tales-title" className={classes.title}>
             <svg className={classes.icon} aria-hidden="true">
               <use href={title + '#title'}></use>
               <span className="visually-hidden">QuizyTales</span>
             </svg>
-            {/* <BrandedLettering
-              positionX={15}
-              positionY={40}
-              boxWidth={295}
-              boxHeight={28}
-              variant="main"
-            /> */}
           </h1>
 
           <p className={classes.text}>
@@ -31,18 +42,14 @@ export const QuizyTales = () => {
             <CustomLink
               to="catalog"
               text="Выбрать готовый квиз"
-              width="285px"
-              fontSize="24px"
-              padding="24px 33px"
+              {...getLinkProps()}
               background="#49969b"
               variant="leftRight"
             />
             <CustomButton
               type="button"
               text="Создать собственный квиз"
-              width="285px"
-              fontSize="24px"
-              padding="28px 22px"
+              {...getButtonProps()}
               background="#49969b"
               variant="author"
             />
--- front/src/ui/Logo/Logo.tsx
+++ front/src/ui/Logo/Logo.tsx
@@ -4,20 +4,13 @@ import classes from './Logo.module.css';
 import logo from '../../assets/sprite.svg';
 
 interface ILogo {
-  width: number;
-  height: number;
   'aria-label'?: string;
 }
 
-export const Logo: FC<ILogo> = ({ width, height, 'aria-label': ariaLabel }) => {
+export const Logo: FC<ILogo> = ({ 'aria-label': ariaLabel }) => {
   return (
     <>
-      <svg
-        className={classes.icon}
-        style={{ width: `${width}px`, height: `${height}px` }}
-        aria-label={ariaLabel}
-        role="img"
-      >
+      <svg className={classes.icon} aria-label={ariaLabel} role="img">
         <use href={logo + '#logo'}></use>
       </svg>
     </>
